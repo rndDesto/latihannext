49e00d557b232763e65d1f9c1ccfe00e1cba5a32
LAPTOP-LJJM05JL\DDB-916644-Widdesto <widdesto.yudistiro@gmail.com>
2022-04-17 16:30:47 +0700

    tambah route about

---
+++ pages/about/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+const about = () => {
+  return (
+    <>
+    <h1>About Page</h1>
+    
+    <Link href="/">back to home</Link>
+    </>
+  )
+}
+
+export default about
