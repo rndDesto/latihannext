2d3c115686b7f75cb72fc2bbc556c3a3aee84b7e
LAPTOP-LJJM05JL\DDB-916644-Widdesto <widdesto.yudistiro@gmail.com>
2022-04-17 17:10:28 +0700

    bug fix button path

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import Button from 'elements/Button'
+import Button from '@/elements/Button'
 import Heading from '@/elements/Heading'
 
 const about = () => {
